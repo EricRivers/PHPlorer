92d0499fe81e7b0363d49ed814342c1b37b7e9a4
Eric Rivers <eric@ericrivers.com>
2014-11-22 11:35:02 -0500

    Testing realpath()
    Added realpath() line to self.php

--- self.php
+++ self.php
@@ -3,6 +3,7 @@
 echo "Current file = " . $_SERVER['PHP_SELF'] . "<br/>\n";
 echo "Current folder with leading separator = " . dirname(urldecode($_SERVER['PHP_SELF'])) . "<br/>\n";
 echo "Current folder = " . basename(dirname(__FILE__)) . "<br/>\n";
+echo "Real Path = " . realpath(dirname(__FILE__)) . "<br/>\n";
 echo "Full path to current folder = " . dirname(__FILE__) . "<br/>\n";
 echo "One level up folder = " . basename(dirname(__DIR__)) . "<br/>\n";
 echo "Path to this folder = " . basename(dirname(__DIR__)) ."\\". basename(dirname(__FILE__)) . "<br/>\n";
